52c425a70b70572dcb3ee56a06220e562e05e270
jinhaEom <umjin98@naver.com>
2022-03-23 22:27:14 +0900

    bottomNavigationbar 추가

--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/MainActivity.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/MainActivity.kt
@@ -2,27 +2,13 @@ package bu.ac.kr.anyfeeling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import bu.ac.kr.anyfeeling.fragment.OneFragment
-import bu.ac.kr.anyfeeling.fragment.TwoFragment
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val menuBar = findViewById<Button>(R.id.menuBar)
-        val settingBar = findViewById<Button>(R.id.settingBar)
 
-        menuBar.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.FrameLayout, OneFragment())
-                .commit()
-        }
-        settingBar.setOnClickListener {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.FrameLayout, TwoFragment())
-                .commit()
-        }
     }
 }
