ebc7c4be14301ccc44b5c974a2c03ccd4c7f3ec5
jinhaEom <umjin98@naver.com>
2022-04-02 15:07:22 +0900

    Homefragment 수정

--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/HomeFragment/HomeFragment.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/HomeFragment/HomeFragment.kt
@@ -1,13 +1,71 @@
 package bu.ac.kr.anyfeeling.HomeFragment
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import bu.ac.kr.anyfeeling.R
+import bu.ac.kr.anyfeeling.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment(R.layout.activity_home) {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
+
+    private var isGatheringMotionAnimating: Boolean = false
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = FragmentHomeBinding.inflate(inflater,container, false)
+        return binding.root
+
+        binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
+            if(binding.scrollView.scrollY > 150f.dpToPx(this).toInt()){
+                if(!isGatheringMotionAnimating){
+                    binding.gatheringDigitalThingsLayout.transitionToStart()
+                }
+            }else{
+                if(!isGatheringMotionAnimating){
+                    binding.gatheringDigitalThingsLayout.transitionToStart()
+                }
+            }
+        }
+        binding.gatheringDigitalThingsLayout.setTransitionListener(object: MotionLayout.TransitionListener{
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+                isGatheringMotionAnimating = true
+            }
+
+            override fun onTransitionChange(
+                motionLayout: MotionLayout?,
+                startId: Int,
+                endId: Int,
+                progress: Float
+            ) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                isGatheringMotionAnimating = false
+            }
+
+            override fun onTransitionTrigger(
+                motionLayout: MotionLayout?,
+                triggerId: Int,
+                positive: Boolean,
+                progress: Float
+            ) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
 
 
 
 }
+fun Float.dpToPx(context: HomeFragment): Float =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,this,context.resources.displayMetrics)
--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/MainActivity.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/MainActivity.kt
@@ -1,14 +1,12 @@
 package bu.ac.kr.anyfeeling
 
 import android.app.Activity
-import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.View
 import android.view.WindowManager
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -21,9 +19,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy{ ActivityMainBinding.inflate(layoutInflater)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
 
 
         val FirstFragment= FirstFragment()
@@ -67,8 +67,7 @@ class MainActivity : AppCompatActivity() {
 
 
 }
-fun Float.dpToPx(context: Context): Float =
-    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,this,context.resources.displayMetrics)
+
 fun Activity.makeStatusBarTransparent() {
     window.apply {
         decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
