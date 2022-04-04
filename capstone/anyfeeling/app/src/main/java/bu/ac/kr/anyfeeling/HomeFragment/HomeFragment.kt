830536fe5a7851bde2ead6485a0f1b29734bd920
jinhaEom <umjin98@naver.com>
2022-04-04 16:28:53 +0900

    toolbar delete

--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/HomeFragment/HomeFragment.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/HomeFragment/HomeFragment.kt
@@ -1,12 +1,18 @@
 package bu.ac.kr.anyfeeling.homeFragment
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import bu.ac.kr.anyfeeling.MainActivity
@@ -14,15 +20,34 @@ import bu.ac.kr.anyfeeling.R
 import bu.ac.kr.anyfeeling.databinding.ActivityMainBinding
 import bu.ac.kr.anyfeeling.databinding.FragmentHomeBinding
 import bu.ac.kr.anyfeeling.dpToPx
+import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.abs
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home){
 
     private var isGatheringMotionAnimating: Boolean = false
 
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?):
+
             View {
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+
+        binding.gatheringDigitalThingsLayout.setTransitionListener(object :
+            MotionLayout.TransitionListener {
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+                isGatheringMotionAnimating = true
+            }
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) {}
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                isGatheringMotionAnimating = false
+            }
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) {}
+
+
+        })
+
+
         binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
             if (binding.scrollView.scrollY > 150f.dpToPx(this).toInt()) {
                 if (isGatheringMotionAnimating.not()) {
@@ -37,36 +62,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
                 }
             }
         }
-        binding.gatheringDigitalThingsLayout.setTransitionListener(object :
-            MotionLayout.TransitionListener {
-            override fun onTransitionStarted(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int
-            ) {
-                isGatheringMotionAnimating = true
-            }
 
-            override fun onTransitionChange(
-                motionLayout: MotionLayout?,
-                startId: Int,
-                endId: Int,
-                progress: Float
-            ) {
-            }
 
-            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
-                isGatheringMotionAnimating = false
-            }
 
-            override fun onTransitionTrigger(
-                motionLayout: MotionLayout?,
-                triggerId: Int,
-                positive: Boolean,
-                progress: Float
-            ) {
-            }
-        })
 
         return binding.root
 
@@ -76,7 +74,11 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
 
 
+
+
+
 }
+
 
 
 
--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/MainActivity.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/MainActivity.kt
@@ -1,17 +1,26 @@
 package bu.ac.kr.anyfeeling
 
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
+import android.view.WindowManager
+
 
 import androidx.fragment.app.Fragment
 import bu.ac.kr.anyfeeling.firstFragment.FirstFragment
 import bu.ac.kr.anyfeeling.homeFragment.HomeFragment
 import bu.ac.kr.anyfeeling.secondFragment.SecondFragment
 import bu.ac.kr.anyfeeling.databinding.ActivityMainBinding
+import bu.ac.kr.anyfeeling.databinding.FragmentHomeBinding
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlin.math.abs
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,43 +32,61 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-
-        /*val FirstFragment= FirstFragment()
+            makeStatusBarTransparent(context = HomeFragment())
+            /*val FirstFragment= FirstFragment()
         val SecondFragment = SecondFragment()
         val HomeFragment = HomeFragment()*/
-        supportFragmentManager.beginTransaction().add(R.id.container,HomeFragment()).commitAllowingStateLoss()
+            supportFragmentManager.beginTransaction().add(R.id.container, HomeFragment())
+                .commitAllowingStateLoss()
 
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_menu)
+            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_menu)
 
-        bottomNavigationView.setOnItemSelectedListener {
-            replaceFragment(
-                when (it.itemId){
-                R.id.first_tab -> FirstFragment()
-                    R.id.second_tab -> SecondFragment()
-                    else -> HomeFragment()
+            bottomNavigationView.setOnItemSelectedListener {
+                replaceFragment(
+                    when (it.itemId) {
+                        R.id.first_tab -> FirstFragment()
+                        R.id.second_tab -> SecondFragment()
+                        else -> HomeFragment()
 
-                }
-            )
+                    }
+                )
 
-            true
-
-
+                true
+            }
         }
 
 
-    }
-    private fun replaceFragment(fragment: Fragment){
-        supportFragmentManager.beginTransaction()
-            .apply {
-                replace(R.id.container,fragment)
-                commitAllowingStateLoss()
-            }
-    }
+        private fun replaceFragment(fragment: Fragment) {
+            supportFragmentManager.beginTransaction()
+                .apply {
+                    replace(R.id.container, fragment)
+                    commitAllowingStateLoss()
+                }
+        }
+
+
+
 
 
 
 }
 
+
 fun Float.dpToPx(context: HomeFragment): Float =
     TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,this,context.resources.displayMetrics)
+
+fun Activity.makeStatusBarTransparent(context: HomeFragment){
+    window.apply {
+        decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        statusBarColor = Color.TRANSPARENT
+    }
+}
+
+
+
+
+
+
