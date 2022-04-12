bcbe7d0e4a4b630025cf577c2f9978f57473864d
jinhaEom <umjin98@naver.com>
2022-04-12 14:31:11 +0900

    firsttab fragment 화면 넘기기 성공

--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/FirstFragment/FirstFragment.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/FirstFragment/FirstFragment.kt
@@ -1,23 +1,33 @@
 package bu.ac.kr.anyfeeling.firstFragment
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentController
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import bu.ac.kr.anyfeeling.R
 import bu.ac.kr.anyfeeling.databinding.FragmentFirsttabBinding
 
-class FirstFragment : Fragment(R.layout.fragment_firsttab) {
+class FirstFragment : Fragment(R.layout.fragment_firsttab)/*, View.OnClickListener*/ {
+    private lateinit var navController: NavController
     private lateinit var mBinding: FragmentFirsttabBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         mBinding = FragmentFirsttabBinding.inflate(inflater, container, false)
+        mBinding.menuHappy.setOnClickListener {
+            val intent = Intent(requireContext(),HappyActivity::class.java)
+            startActivity(intent)
+        }
 
         return mBinding.root
     }
