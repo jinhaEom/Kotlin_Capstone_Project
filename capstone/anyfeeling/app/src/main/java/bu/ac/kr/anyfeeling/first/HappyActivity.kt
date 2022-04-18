3a1a7e9c742cf7eafd2056e2a906847a5e39c98f
jinhaEom <umjin98@naver.com>
2022-04-18 14:51:25 +0900

    재생목록에서 노래 재생 성공

--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/first/HappyActivity.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/first/HappyActivity.kt
@@ -1,5 +1,6 @@
 package bu.ac.kr.anyfeeling.first
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
@@ -12,8 +13,12 @@ import bu.ac.kr.anyfeeling.PlayerModel
 import bu.ac.kr.anyfeeling.R
 import bu.ac.kr.anyfeeling.databinding.FragmentPlayerBinding
 import bu.ac.kr.anyfeeling.service.MusicDto
+import bu.ac.kr.anyfeeling.service.MusicModel
 import bu.ac.kr.anyfeeling.service.MusicService
 import bu.ac.kr.anyfeeling.service.mapper
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,6 +31,7 @@ class HappyActivity: AppCompatActivity(R.layout.fragment_player) {
     private var model : PlayerModel = PlayerModel()
     private var binding : FragmentPlayerBinding? = null
     private var isWatchingPlayListView = true
+    private var player : SimpleExoPlayer?= null
     private lateinit var playListAdapter: PlayListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,10 +41,55 @@ class HappyActivity: AppCompatActivity(R.layout.fragment_player) {
         val fragmentPlayerBinding = FragmentPlayerBinding.inflate(layoutInflater)
         binding = fragmentPlayerBinding
         setContentView(binding!!.root)
+        initPlayView(fragmentPlayerBinding)
         initPlayListButton(fragmentPlayerBinding)
+        initPlayControlButtons(fragmentPlayerBinding)
         initRecyclerView(fragmentPlayerBinding)
         getVideoListFromServer()
 
+    }
+
+    private fun initPlayControlButtons(fragmentPlayerBinding: FragmentPlayerBinding) {
+        fragmentPlayerBinding.playControlImageView.setOnClickListener {
+            val player = this.player?: return@setOnClickListener
+
+            if(player.isPlaying){
+                player.pause()
+            }else{
+                player.play()
+            }
+        }
+        fragmentPlayerBinding.skipNextImageView.setOnClickListener {
+
+        }
+        fragmentPlayerBinding.skipPrevImageView.setOnClickListener {
+
+        }
+    }
+
+    private fun initPlayView(fragmentPlayerBinding: FragmentPlayerBinding) {
+        this?.let{
+            player = SimpleExoPlayer.Builder(this).build()
+        }
+
+        fragmentPlayerBinding.playerView.player = player
+
+        binding.let { binding ->
+
+            player?.addListener(object: Player.EventListener{
+
+                override fun onIsPlayingChanged(isPlaying: Boolean) {
+                    //플레이어가 재생이 될때 or 일시정지될때 callback으로 내려오는 함수
+                    super.onIsPlayingChanged(isPlaying)
+                    if(isPlaying){
+                        binding?.playControlImageView?.setImageResource(R.drawable.ic_baseline_pause_24)
+                    }else{
+                        binding?.playControlImageView?.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+                    }
+                }
+            })
+
+        }
     }
 
     private fun initRecyclerView(fragmentPlayerBinding: FragmentPlayerBinding) {
@@ -80,6 +131,7 @@ class HappyActivity: AppCompatActivity(R.layout.fragment_player) {
                                 val modelList = it.musics.mapIndexed{ index, musicEntity ->
                                     musicEntity.mapper(index.toLong())
                                 }
+                                setMusicList(modelList)
                                 playListAdapter.submitList(modelList)
                             }
                         }
@@ -88,6 +140,18 @@ class HappyActivity: AppCompatActivity(R.layout.fragment_player) {
 
                     })
             }
+    }
+    private fun setMusicList(modelList: List<MusicModel>){
+        this?.let{
+            player?.addMediaItems(modelList.map { musicModel ->
+                MediaItem.Builder()
+                    .setMediaId(musicModel.id.toString())
+                    .setUri(musicModel.streamUrl)
+                    .build()
+            })
+            player?.prepare()
+            player?.play()
+        }
     }
 
 }
