d862d904ddd211faa41398de46e827b4baaf315b
jinhaEom <umjin98@naver.com>
2022-04-27 15:11:02 +0900

    alertdialog 화면 구성

--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/SecondFragment/SecondFragment.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/SecondFragment/SecondFragment.kt
@@ -1,8 +1,35 @@
 package bu.ac.kr.anyfeeling.secondFragment
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.widget.AlertDialogLayout
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import bu.ac.kr.anyfeeling.R
+import bu.ac.kr.anyfeeling.databinding.FragmentFirsttabBinding
+import bu.ac.kr.anyfeeling.databinding.FragmentSecondtabBinding
 
-class SecondFragment: Fragment(R.layout.fragment_secondtab){
+class SecondFragment: DialogFragment(R.layout.fragment_secondtab) {
+    private lateinit var mBinding: FragmentSecondtabBinding
+    /*override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mBinding = FragmentSecondtabBinding.inflate(inflater, container, false)
+        val view = mBinding.root
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        mBinding.
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }*/
+
+
 
 }
