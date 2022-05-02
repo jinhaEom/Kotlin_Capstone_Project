889cc1929e561f2bb4e9483284522c8ec41b8d86
jinhaEom <umjin98@naver.com>
2022-05-02 16:39:15 +0900

    회원가입 패스워드 check 활성화

--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/SignUpActivity.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/SignUpActivity.kt
@@ -1,14 +1,19 @@
 package bu.ac.kr.anyfeeling
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.Checkable
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.internal.RemoveFirstDesc
 
 class SignUpActivity : AppCompatActivity() {
     private var auth: FirebaseAuth? = null
@@ -28,54 +33,50 @@ class SignUpActivity : AppCompatActivity() {
         val signupOk = findViewById<Button>(R.id.signupOk)
 
 
+
+
         signupOk.setOnClickListener {
-            createAccount(email.text.toString(), password.text.toString())
+            createAccount(
+                email.text.toString(),
+                password.text.toString(),
+                passwordCheck.text.toString()
+            )
         }
-        /*if(password!=passwordCheck){
-
-            password.setText("")
-            Toast.makeText(this,"비밀번호가 틀립니다.",Toast.LENGTH_SHORT).show()
-
-        }*/
 
         cancel()
-        /* password.addTextChangedListener(object : TextWatcher{
-             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-
-             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                 if(password.text.toString()==passwordCheck.text.toString()){
-                     LoginButton.isEnabled = true
-
-                 }
-             }
-
-             override fun afterTextChanged(s: Editable?) {}
-
-         })*/
 
 
     }
 
-    private fun createAccount(email: String, password: String) {
+
+    private fun createAccount(email: String, password: String, passwordCheck: String) {
+        val signupOk = findViewById<Button>(R.id.signupOk)
         if (email.isNotEmpty() && password.isNotEmpty()) {
-            auth?.createUserWithEmailAndPassword(email, password)
-                ?.addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(
-                            this,
-                            "회원가입에 성공했습니다. 로그인 버튼을눌러 로그인 해주세요.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        finish()
-                    } else {
-                        Toast.makeText(this, "이미 가입한 이메일이거나, 회원가입에 실패했습니다.", Toast.LENGTH_SHORT)
-                            .show()
+            if (password != passwordCheck) {
+                Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
 
+            } else {
+                auth?.createUserWithEmailAndPassword(email, password)
+                    ?.addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            signupOk.isEnabled = true
+                            Toast.makeText(
+                                this,
+                                "회원가입에 성공했습니다. 로그인 버튼을눌러 로그인 해주세요.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            finish()
+                        }
                     }
+            }
 
-                }
+
+        } else {
+            Toast.makeText(this, "이메일이 이미 존재하거나 회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show()
 
         }
+
 
     }
 
