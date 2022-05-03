5250a9296c95f61d3d5979771a4a7c60fa7b50c0
jinhaEom <umjin98@naver.com>
2022-05-03 22:09:37 +0900

    noti 제작중

--- capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/MainActivity.kt
+++ capstone/anyfeeling/app/src/main/java/bu/ac/kr/anyfeeling/MainActivity.kt
@@ -1,17 +1,24 @@
 package bu.ac.kr.anyfeeling
 
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Notification
 import android.app.NotificationManager
 import android.content.Context
+import android.drm.DrmStore
 import android.graphics.Color
 import android.media.MediaMetadata
+import android.media.MediaSession2
 import android.media.session.MediaSession
+import android.media.session.PlaybackState
 import android.os.Build
+import android.os.Build.VERSION_CODES.O
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.System.getString
+import android.support.v4.media.session.MediaSessionCompat
+import android.support.v4.media.session.PlaybackStateCompat
 import android.util.TypedValue
 import android.view.View
 import android.view.WindowManager
@@ -19,10 +26,13 @@ import androidx.core.app.NotificationCompat
 
 
 import androidx.fragment.app.Fragment
+import androidx.media.session.MediaButtonReceiver
 import bu.ac.kr.anyfeeling.first.FirstFragment
 import bu.ac.kr.anyfeeling.homeFragment.HomeFragment
 import bu.ac.kr.anyfeeling.secondFragment.SecondFragment
 import bu.ac.kr.anyfeeling.databinding.ActivityMainBinding
+import com.bumptech.glide.Priority
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.util.NotificationUtil.createNotificationChannel
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -30,11 +40,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    lateinit var playpause : Notification.Action
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var exoPlayer : SimpleExoPlayer
+    var btnActions: Array<Notification.Action?> = arrayOfNulls<Notification.Action>(3)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+
+        gc
 
 
 
@@ -102,6 +119,67 @@ class MainActivity : AppCompatActivity() {
                 replace(R.id.container, fragment)
                 commitAllowingStateLoss()
             }
+    }
+    @SuppressLint("WrongConstant")
+    private fun getNotification(playbackState: Int):NotificationCompat.Builder{
+
+        val controller = mediaSession.controller
+        val mediaMetadata = controller.metadata
+        val description = mediaMetadata.description
+        val CHANNEL_ID = "AnyFeelingNoti"
+
+
+
+        if(playbackState == PlaybackStateCompat.STATE_PLAYING){
+            addAction(
+                NotificationCompat(
+                    R.drawable.ic_baseline_pause_24,
+                    getString("미미"),
+                    MediaButtonReceiver.buildMediaButtonPendingIntent(
+                        applicationContext, PlaybackStateCompat.ACTION_PLAY_PAUSE
+                    )
+                )
+            )
+        }else{
+
+        }
+        val previous =
+            Notification.Action.Builder(
+                R.drawable.ic_baseline_skip_previous_24,
+                getString(R.string.app_name),
+                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                    this, PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS
+                )
+            ).build()
+        val next =
+            Notification.Action.Builder(
+                R.drawable.ic_baseline_skip_next_24,
+                getString(R.string.app_name),
+                MediaButtonReceiver.buildMediaButtonPendingIntent(
+                    this, PlaybackStateCompat.ACTION_SKIP_TO_NEXT
+                )
+            ).build()
+        if(playbackState == PlaybackStateCompat.STATE_PLAYING){
+            playpause =
+                Notification.Action.Builder(
+                    R.drawable.ic_baseline_play_arrow_24,
+                    getString(R.string.app_name),
+                    MediaButtonReceiver.buildMediaButtonPendingIntent(
+                        this,PlaybackStateCompat.ACTION_PLAY_PAUSE
+                    )
+                ).build()
+        }else{
+            playpause =
+                Notification.Action.Builder(
+                    R.drawable.ic_baseline_pause_24,
+                    getString(R.string.app_name),
+                    MediaButtonReceiver.buildMediaButtonPendingIntent(
+                        this,PlaybackStateCompat.ACTION_PLAY_PAUSE
+                    )
+                ).build()
+        }
+
+
     }
 }
 
