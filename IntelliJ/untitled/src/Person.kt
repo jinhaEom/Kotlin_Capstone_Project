c18a3629648c36482a496082e057218026876ac9
Eom Jinha <84216838+jinhaEom@users.noreply.github.com>
2022-07-01 15:08:39 +0900

    Delete IntelliJ directory

--- IntelliJ/untitled/src/Person.kt
+++
@@ -1,7 +0,0 @@
-data class Person(val name: String, val age: Int)
-
-fun main() {
-    val person1 = Person(name = "tony", age = 12)
-    val person2 = Person(name = "tony", age = 12)
-    println(person2==person1)
-}
